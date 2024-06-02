a67080cb4c177963ed0287891c67660696dedb7f
AbdalrhmanAmmar <bodyty2n@gmail.com>
2024-06-02 14:40:24 +0200

    ErrorPagedynamic

--- Frontend/src/routes/index.tsx
+++ Frontend/src/routes/index.tsx
@@ -7,15 +7,32 @@ import MainLayout from "../layouts/MainLayout/MainLayout";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Categories from "../pages/Categories";
+import Login from "./../pages/Login";
+import Register from "../pages/Register";
+import ErrorPage from "../pages/ErrorPage";
+import Products from "../pages/Products";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<MainLayout />}>
+      <Route path="/" element={<MainLayout />} errorElement={<ErrorPage />}>
         <Route index element={<Home />} />
         <Route path="About" element={<About />} />
         <Route path="Categories" element={<Categories />} />
-        <Route path="About" element={<About />} />
+        <Route
+          path="Products/:prefix"
+          element={<Products />}
+          loader={({ params }) => {
+            if (!/^[a-z]+$/i.test(params.prefix as string)) {
+              throw new Response("bad request", {
+                statusText: "Category not Found",
+                status: 400,
+              });
+            } else return true;
+          }}
+        />
+        <Route path="Login" element={<Login />} />
+        <Route path="Register" element={<Register />} />
       </Route>
     </>
   )
